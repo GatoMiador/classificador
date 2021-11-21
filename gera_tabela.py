dc16707d09134e1736731ebc8325182ba61db928
Marcos Vicente Cruz <marcos@gatomiador.net>
2021-11-21 15:56:56 -0300

    Modificada a geração da tabela do esmeril para incluir o in rush.

--- gera_tabela.py
+++ gera_tabela.py
@@ -19,7 +19,7 @@ n.load_table(path, 'laptop', 'P', 50, 60, size)
 n.load_table(path, 'luminaria', 'P', 1, 2, size)
 n.load_table(path, 'nada', 'P', 4, 6, size)
 n.load_table(path, 'osciloscópio', 'P', 10, 20, size)
-n.load_table(path, 'esmeril', 'P', 60, 90, size)
+n.load_table(path, 'esmeril', 'P', 60, 295, size)
 n.load_table(path, 'ferro_de_solda', 'P', 160, 180, size)
 
 # Salva
