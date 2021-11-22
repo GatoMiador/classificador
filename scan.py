4c61b749d220df073e1144864dba7c549db10023
Marcos Vicente Cruz <marcos@gatomiador.net>
2021-11-22 10:47:57 -0300

    Corrigida a deteção de carga na estabilização.

--- scan.py
+++ scan.py
@@ -34,101 +34,106 @@ for chunk in pd.read_csv(path + "multi2_d.csv", \
                 step = 1
         elif step == 1: # Verifica se a variação é maior que o threshold
             d = abs(row['I'] - ac.I)
-            if d > delta:
-                I0 = row['I']
+            if d >= delta:
                 n0 = n
                 step = 2
-        elif step == 2: # Espera por 200ms
+        elif step == 2: # Espera estabilizar por 100ms
             # Só classifica se a corrente se mantém acima do  delta pelo tempo
             d = abs(row['I'] - ac.I)
-            if d > delta:
-                if (n - n0) >= (cycles*12):
-                    # Verifica se estabilizou
-                    d = abs(row['I'] - I0)
-                    if d < delta:
-                        # Faz a detecção de carga se estabilizou
-                        d = row['I'] - ac.I
-                        if d >= 0:
-                            # Remove a carga 'nada' que interfere
-                            if len(cargas) == 1:
-                                if cargas[0].nome == 'nada':
-                                    ac = nd.Carga()
-                                    del cargas[0]
-                            # Calcula a carga que entrou e classifica ela
-                            l = nd.Carga()
-                            l.ini = n;
-                            l.P = row['P'] - ac.P
-                            l.Q = row['Q'] - ac.Q
-                            l.D = row['D'] - ac.D
-                            l.I = row['I'] - ac.I
-                            l.V = row['V']
-                            l.calc_factors()
-                            l.nome = ia.classify(l)
-                            if l.nome != "nada":
-                                print("carga entrou:", l.nome, ' -->', n)
-                                # Seta o novo patamar, se a carga não foi 'nada'
-                                ac.P = row['P']
-                                ac.Q = row['Q']
-                                ac.D = row['D']
-                                ac.V = row['V']
-                                ac.I = row['I']
-                            else: 
-                                # Se a carga classificada é 'nada' e temos 
-                                # cargas ativas, zera os valores para que ela 
-                                # seja detectada. Isso acontece porque a carga
-                                # 'nada' é *maior* que a menor carga possível
-                                if len(cargas) > 0:
-                                    ac = nd.Carga()
-                            cargas.append(l)
-                        else:
-                            # Calcula a carga que saiu e classifica ela
-                            o = nd.Carga()
-                            o.fim = n;
-                            o.P = ac.P - row['P']
-                            o.Q = ac.Q - row['Q']
-                            o.D = ac.D - row['D']
-                            o.I = ac.I - row['I']
-                            o.V = row['V']
-                            o.calc_factors()
-                            o.nome = ia.classify(o)
-                            # Procura pela carga que saiu
-                            index = -1
-                            i = 0
-                            for k in cargas:
-                                if k.nome == o.nome:
-                                    index = i
-                                    break
-                                i = i + 1
-                            if index >= 0:
-                                print("carga saiu:", cargas[index].nome,
-                                      ' -->', n)
-                                ac = nd.Carga()
-                                if cargas[index].nome != 'nada':
-                                    # Recalcula o thresold
-                                    for o in cargas:
-                                        if o != cargas[index]:
-                                            ac.P = ac.P + o.P
-                                            ac.Q = ac.Q + o.Q
-                                            ac.D = ac.D + o.D
-                                            ac.I = ac.I + o.I
-                                    ac.V = row['V']
-                                # Atualiza, remove e coloca as cargas no relatório
-                                cargas[index].fim = n
-                                report.append(cargas[index])
-                                cargas.remove(cargas[index])
-                            else:
-                                # Adiciona a falsa detecção no relatório
-                                o.ini = n
-                                o.fim = n
-                                o.falso=True
-                                report.append(o)
-                                print("detecção falsa :", o.nome)
-                        step = 1; # Volta a procurar por mudamças
-                    else:
-                        I0 = row['I'] # Aguarda a estabilização
-                        n0 = n
+            if d >= delta:
+                if (n - n0) >= (cycles*6):
+                    I0 = row['I']
+                    n0 = n
+                    step = 3
             else:
                 step = 1 # Volta para o passo 1 se baixou
+        elif step == 3: # Espera estabilizar por 100ms
+            # Verifica se estabilizou
+            d = abs(row['I'] - I0)
+            if d < delta:
+                if (n - n0) >= (cycles*6):
+                    hora = n - cycles * 12
+                    # Faz a detecção de carga se estabilizou
+                    d = row['I'] - ac.I
+                    if d >= 0:
+                        # Remove a carga 'nada' que interfere
+                        if len(cargas) == 1:
+                            if cargas[0].nome == 'nada':
+                                ac = nd.Carga()
+                                del cargas[0]
+                        # Calcula a carga que entrou e classifica ela
+                        l = nd.Carga()
+                        l.ini = hora;
+                        l.P = row['P'] - ac.P
+                        l.Q = row['Q'] - ac.Q
+                        l.D = row['D'] - ac.D
+                        l.I = row['I'] - ac.I
+                        l.V = row['V']
+                        l.calc_factors()
+                        l.nome = ia.classify(l)
+                        if l.nome != "nada":
+                            print("carga entrou:", l.nome, ' -->', hora)
+                            # Seta o novo patamar, se a carga não foi 'nada'
+                            ac.P = row['P']
+                            ac.Q = row['Q']
+                            ac.D = row['D']
+                            ac.V = row['V']
+                            ac.I = row['I']
+                        else: 
+                            # Se a carga classificada é 'nada' e temos 
+                            # cargas ativas, zera os valores para que ela 
+                            # seja detectada. Isso acontece porque a carga
+                            # 'nada' é *maior* que a menor carga possível
+                            if len(cargas) > 0:
+                                ac = nd.Carga()
+                        cargas.append(l)
+                    else:
+                        # Calcula a carga que saiu e classifica ela
+                        o = nd.Carga()
+                        o.fim = hora;
+                        o.P = ac.P - row['P']
+                        o.Q = ac.Q - row['Q']
+                        o.D = ac.D - row['D']
+                        o.I = ac.I - row['I']
+                        o.V = row['V']
+                        o.calc_factors()
+                        o.nome = ia.classify(o)
+                        # Procura pela carga que saiu
+                        index = -1
+                        i = 0
+                        for k in cargas:
+                            if k.nome == o.nome:
+                                index = i
+                                break
+                            i = i + 1
+                        if index >= 0:
+                            print("carga saiu:", cargas[index].nome,
+                                  ' -->', hora)
+                            ac = nd.Carga()
+                            if cargas[index].nome != 'nada':
+                                # Recalcula o thresold
+                                for o in cargas:
+                                    if o != cargas[index]:
+                                        ac.P = ac.P + o.P
+                                        ac.Q = ac.Q + o.Q
+                                        ac.D = ac.D + o.D
+                                        ac.I = ac.I + o.I
+                                ac.V = row['V']
+                            # Atualiza, remove e coloca as cargas no relatório
+                            cargas[index].fim = hora
+                            report.append(cargas[index])
+                            cargas.remove(cargas[index])
+                        else:
+                            # Adiciona a falsa detecção no relatório
+                            o.ini = hora
+                            o.fim = hora
+                            o.falso=True
+                            report.append(o)
+                            print("detecção falsa :", o.nome)
+                    step = 1; # Volta a procurar por mudamças                    
+            else:
+                # Volta se a entrada se desestabiizou
+                step = 2
         else:
             print('default')
 
