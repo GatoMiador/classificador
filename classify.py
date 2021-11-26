87c97a8ce884ad92d0247f55c374dfbc5ff16daf
Marcos Vicente Cruz <marcos@gatomiador.net>
2021-11-25 21:57:58 -0300

    Removidos os workarounds, adicionados pesos, outliers, agora funciona.

--- classify.py
+++ classify.py
@@ -56,14 +56,36 @@ class Normal:
         self.params.clear();
 
     # Carrega uma tabela com dados para ensinar a IA
-    def load_table(self, path, nome, field, ini, fim, num, f=None):
+    def load_table(self, path, nome, outliers, start, stop, size, f=None):
+        # Carrega o dataset
         n = path + nome + "_d.csv"
-        data = pd.read_csv(n, usecols=self.inputs)
-        data = data.drop(data[data[field] < ini].index)
-        data = data.drop(data[data[field] > fim].index)
-        sample = data.sample(num)
+        data = pd.read_csv(n, usecols=self.inputs, nrows=stop)
+        # Calcula os mínimos e máximos
+        minvals = data.min(axis=0)
+        maxvals = data.max(axis=0)
+        if hasattr(self, 'minvals') == False:
+            self.minvals = copy.copy(minvals)
+        if hasattr(self, 'maxvals') == False:
+            self.maxvals = copy.copy(maxvals)
+        for (a, b) in zip(self.minvals, minvals):
+            a = min(a, b)
+        for (a, b) in zip(self.maxvals, maxvals):
+            a = max(a, b)
+        # Carrega a tabela
+        data.drop(range(start,), inplace=True)
+        # Remove os outliers
+        for field in outliers:
+            data = data.drop(data[data[field] < outliers[field][0] ].index)
+            data = data.drop(data[data[field] > outliers[field][1] ].index)
+        if data.shape[0] < size:
+            raise ValueError('Dados insuficientes após a remoção dos outliers,'
+                ' arquivo ', nome, '.')
+        # Pega as amostras aleatórias
+        sample = data.sample(size)
+        # Adiciona as features
         sample[self.features[0]] = \
             [ f ] * len(sample.index)
+        # Adiciona o nome da carga
         sample[self.outputs[0] ] = [ nome ] * len(sample.index)
         self.table = self.table.append(sample)
         return
@@ -97,13 +119,13 @@ class Normal:
                 if e in inputs:
                     inputs.remove(e)
             for o in inputs:
-                mn = min(f.table[o])
-                f.table[o] = f.table[o] - mn
-                mx = max(f.table[o])
-                f.table[o] = f.table[o] / mx
-                f.params[o] = [ mn, mx ]
+                mini = f.minvals[o]
+                maxi = (f.maxvals[o] - f.minvals[o]) / 10
+                f.table[o] = f.table[o] - mini
+                f.table[o] = f.table[o] / maxi
+                f.params[o] = [ mini, maxi ]
             # Trata os fatores de potência e etc. como casos especiais
-            # seus valores já estão entre 0 e 1
+            # já que seus valores já estão entre 0 e 1
             f.params['fp'] = [ 0, 1 ]
             f.params['fl'] = [ 0, 1 ]
             # O fator de reatividade está sempre entre -1 e 1, normaliza
@@ -111,6 +133,8 @@ class Normal:
             f.table['fr'] = f.table['fr'] - f.params['fr'][0]
             f.table['fr'] = f.table['fr'] / f.params['fr'][1]
         # Salva os parâmetros que a IA vai usar
+        del f.minvals
+        del f.maxvals
         with open(arquivo + ".data", 'wb') as outp:
             pickle.dump(f, outp, pickle.HIGHEST_PROTOCOL)
             outp.close()
@@ -139,29 +163,26 @@ class Type(Enum):
 class IA:
     def __init__(self, f, type=Type.KNN, pca=True):
         self.n = Normal.load(f)
-        z = copy.copy(self.n.table)
-        z.insert(0, 'X', 0)
-        learn = ['X'] + Normal.learning_data
         # Faz a PCA
         self.pca = pca
         if self.pca == True:
             self.p = PCA(n_components=3)
-            X = self.p.fit_transform(z[ learn ].values)
-            print('PCA:', len(z[ learn ].columns)-1, '-->', len(X[0]) )
+            X = self.p.fit_transform(self.n.table[Normal.learning_data].values)
+            print('PCA:',
+                  len(self.n.table[ Normal.learning_data ].columns),
+                  '-->',
+                  len(X[0]) )
         else:
-            X = z[ learn ].values
+            X = self.n.table[Normal.learning_data].values
         # Faz a classificação de acordo com os dados de aprendizado
         if type == Type.KNN:
-            self.__predictor = KNeighborsClassifier(n_neighbors=1)
-            self.__predictor.fit(X, z[Normal.outputs[0] ])
+            self.predictor = KNeighborsClassifier(n_neighbors=1)
+            self.predictor.fit(X, self.n.table[Normal.outputs[0] ])
         elif type == Type.NEURAL:
-            self.__predictor = MLPClassifier(
+            self.predictor = MLPClassifier(
                 solver='lbfgs', alpha=1e-5,
                 hidden_layer_sizes=(13, 2), random_state=1)
-            self.__predictor.fit(X, z[Normal.outputs[0] ])
-        # Remove dados não utilizados da tabela
-        for o in self.n.inputs:
-            self.n.table.drop(o, axis=1, inplace=True)
+            self.__predictor.fit(X, self.n.tablez[Normal.outputs[0] ])
 
     # Faz a classificação dos dados de carga
     def classify(self, l):
@@ -173,15 +194,16 @@ class IA:
             'fp': l.fp,
             'fl': l.fl,
             'fr': l.fr })
-        # Envia para a IA para encontrar o número da carga
-        z = [ 0 ]
+        z = [ ]
         for n in Normal.learning_data:
             z = z + [ r[n] ]
+        # Faz a PCA caso habilitada
         if self.pca == True:
             z = self.p.transform([z])
         else:
             z = [ z ]
-        n = self.__predictor.predict(z)[0]
+        # Envia para a IA para encontrar o número da carga
+        n = self.predictor.predict(z)[0]
         # Devolve as peculiaridades da carga
         f = self.n.table[self.n.table[self.n.outputs[0] ] == n] \
             [self.n.features[0]].head(1).values[0]
--- gera_tabela.py
+++ gera_tabela.py
@@ -10,17 +10,53 @@ import classify as nd
 
 path = "../"
 
-size=1000
+size=10
 
 n = nd.Normal();
 
 # Carrega as tabelas com os limites
-n.load_table(path, 'laptop', 'P', 50, 60, size)
-n.load_table(path, 'luminaria', 'P', 1, 2, size)
-n.load_table(path, 'nada', 'P', 4, 6, size)
-n.load_table(path, 'osciloscópio', 'P', 10, 20, size)
-n.load_table(path, 'esmeril', 'P', 60, 295, size, { 'inrush': True } )
-n.load_table(path, 'ferro_de_solda', 'P', 160, 180, size)
+outliers = {
+    'P' : [ 43, 57 ],
+    'Q' : [ -15.91, -13.17 ],
+    'D' : [ 120.3, 150.3 ],
+}
+n.load_table(path, 'laptop', outliers, 2799000, 11000000, size)
+outliers = {
+    'P' : [ 1.12, 1.45 ],
+    'Q' : [ -3.1, -2.819 ],
+    'D' : [ 17.29, 19.98 ],
+}
+n.load_table(path, 'luminaria', outliers, 2435000, 11000000, size)
+outliers = {
+    'P' : [ 4.66, 5.226 ],
+    'Q' : [ -3.722, -3.222 ],
+    'D' : [ 13.48, 17.77 ],
+}
+n.load_table(path, 'nada', outliers, 2435000, 11000000, size)
+outliers = {
+    'P' : [ 13.87, 16.91 ],
+    'Q' : [ -7.777, -5.671 ],
+    'D' : [ 36.13, 43.85 ],
+}
+n.load_table(path, 'osciloscópio', outliers, 884100, 11000000, size)
+outliers = {
+    'P' : [ 75.24, 78.39 ],
+    'Q' : [ 77.99, 80.48 ],
+    'D' : [ 11.35, 12.37 ],
+}
+n.load_table(path, 'esmeril', outliers, 2800000, 11000000, size, { 'inrush': True } )
+outliers = {
+    'P' : [ 280, 300 ],
+    'Q' : [ 94.67, 97.36 ],
+    'D' : [ 12.09, 14.66 ],
+}
+n.load_table(path, 'esmeril', outliers, 2176000, 2238000, size, { 'inrush': True } )
+outliers = {
+    'P' : [ 166, 176 ],
+    'Q' : [ -10.35, -9.065 ],
+    'D' : [ 10.17, 11.15 ],
+}
+n.load_table(path, 'ferro_de_solda', outliers, 2098000, 11000000, size)
 
 # Salva
 nd.Normal.save(path + "out_table", n)
